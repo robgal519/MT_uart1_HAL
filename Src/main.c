45a985ae6c0d526a093364badbb5dab49ca6e1ab
= <=>
2020-09-30 00:28:14 +0200

    added calibration

--- Src/main.c
+++ Src/main.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -27,8 +27,9 @@
 #define false 0
 #define true 1
 #include "stdio.h"
-#include "string.h"
 #include "stdlib.h"
+#include "string.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,10 +48,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart1;
-UART_HandleTypeDef huart2;
 DMA_HandleTypeDef hdma_usart1_tx;
-DMA_HandleTypeDef hdma_usart2_tx;
-DMA_HandleTypeDef hdma_usart2_rx;
 
 /* USER CODE BEGIN PV */
 
@@ -61,7 +59,6 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART1_UART_Init(void);
-static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -69,13 +66,12 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-void log_message(char* ch){
-    while(*ch)
-        ITM_SendChar(*ch++);
+void log_message(char *ch) {
+  while (*ch)
+    ITM_SendChar(*ch++);
 }
 
-void init_Hal_UART(UART_HandleTypeDef *huart, size_t baudrate)
-{
+void init_Hal_UART(UART_HandleTypeDef *huart, size_t baudrate) {
   huart->Instance = USART1;
   huart->Init.BaudRate = baudrate;
   huart->Init.WordLength = UART_WORDLENGTH_8B;
@@ -84,13 +80,11 @@ void init_Hal_UART(UART_HandleTypeDef *huart, size_t baudrate)
   huart->Init.Mode = UART_MODE_TX_RX;
   huart->Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart->Init.OverSampling = UART_OVERSAMPLING_8;
-  if (HAL_UART_Init(huart) != HAL_OK)
-  {
+  if (HAL_UART_Init(huart) != HAL_OK) {
     Error_Handler();
   }
 }
-void initGPIOA()
-{
+void initGPIOA() {
   static GPIO_InitTypeDef outputPins;
   outputPins.Pin = GPIO_PIN_4;
   outputPins.Mode = GPIO_MODE_OUTPUT_PP;
@@ -102,127 +96,168 @@ void initGPIOA()
 }
 
 static uint8_t dummy[500];
-void randomizeData(uint8_t *buffor, size_t size)
-{
-  for (size_t i = 0; i < size; i++)
-  {
+void randomizeData(uint8_t *buffor, size_t size) {
+  for (size_t i = 0; i < size; i++) {
     buffor[i] = rand() % ((uint8_t)(-1));
   }
 }
 volatile bool UART_TransferComplete = false;
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
-{
-  if (huart == &huart2 || huart == &huart1)
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+  if (huart == &huart1)
     UART_TransferComplete = true;
 }
 
-size_t testBaudrateHAL(size_t baudrate, UART_HandleTypeDef *uart)
-{
-  static size_t counter = 0;
+void TIM2_IRQHandler(void) {
+  // clear interrupt status
+  if (TIM2->DIER & 0x01) {
+    if (TIM2->SR & 0x01) {
+      TIM2->SR &= (uint32_t) ~(1U << 0);
+    }
+  }
+  TIM2->CR1 &= (~((uint16_t)TIM_CR1_CEN));
+  UART_TransferComplete = true;
+  NVIC_DisableIRQ(TIM2_IRQn);
+}
 
-  init_Hal_UART(uart, baudrate);
-  counter = 0;
+void setup_timer1(UART_HandleTypeDef *uart, size_t baudrate) {
+  (void)uart;
+  (void)baudrate;
+  RCC->APB1ENR |= (1 << 0);
+
+  // Timer clock runs at ABP1 * 2
+  //   since ABP1 is set to /4 of fCLK
+  //   thus 168M/4 * 2 = 84Mhz
+  // set prescaler to 83999
+  //   it will increment counter every prescalar cycles
+  // fCK_PSC / (PSC[15:0] + 1)
+  // 84 Mhz / 8399 + 1 = 10 khz timer clock speed
+  TIM2->PSC = 0;
+
+  // Set the auto-reload value to 10000
+  //   which should give 1 second timer interrupts
+  TIM2->ARR = 84000000;
+
+  // Update Interrupt Enable
+  TIM2->DIER |= (1 << 0);
+
+  NVIC_SetPriority(TIM2_IRQn, 2); // Priority level 2
+  // enable TIM2 IRQ from NVIC
+  NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+HAL_StatusTypeDef start_timer(UART_HandleTypeDef *uart, uint8_t *data,
+                              uint16_t size) {
+  (void)uart;
+  (void)data;
+  (void)size;
+
+  TIM2->CR1 |= (1 << 0);
+  return HAL_OK;
+}
+
+HAL_StatusTypeDef dummy_deinit(UART_HandleTypeDef *huart) {
+  (void)huart;
+  return HAL_OK;
+}
+
+struct test_ctx {
+  UART_HandleTypeDef *uart;
+  void (*configure)(UART_HandleTypeDef *uart, size_t baudrate);
+  HAL_StatusTypeDef (*transfer)(UART_HandleTypeDef *uart, uint8_t *data,
+                                uint16_t size);
+  HAL_StatusTypeDef (*deinit)(UART_HandleTypeDef *huart);
+};
+
+struct test_ctx test_dma_ctx = {
+    .uart = &huart1,
+    .configure = init_Hal_UART,
+    .transfer = HAL_UART_Transmit_DMA,
+    .deinit = HAL_UART_DeInit,
+};
+
+struct test_ctx test_it_ctx = {
+    .uart = &huart1,
+    .configure = init_Hal_UART,
+    .transfer = HAL_UART_Transmit_IT,
+    .deinit = HAL_UART_DeInit,
+};
+
+struct test_ctx calibrate_ctx = {
+    .uart = NULL,
+    .configure = setup_timer1,
+    .transfer = start_timer,
+    .deinit = dummy_deinit,
+};
+
+bool test_performance(struct test_ctx *ctx, uint32_t baud, uint32_t *counter) {
+  uint32_t cnt = 0;
+
+  if (ctx == NULL)
+    return false;
+  if (ctx->configure == NULL)
+    return false;
+
+  ctx->configure(ctx->uart, baud);
+
   UART_TransferComplete = false;
   randomizeData(dummy, sizeof(dummy));
-  HAL_StatusTypeDef status = HAL_UART_Transmit_IT(uart, dummy, sizeof(dummy));
-  if (status != HAL_OK)
-  {
-    // Error !!!
+  if (ctx->transfer(ctx->uart, dummy, sizeof(dummy)) != HAL_OK)
+    return false;
+
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
+  while (!UART_TransferComplete) {
+    cnt++;
   }
-  else
-  {
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-    while (!UART_TransferComplete)
-    {
-      counter++;
-    }
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
-  }
-  HAL_UART_DeInit(uart);
-  return counter;
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
+
+  if (ctx->deinit == NULL)
+    return false;
+  ctx->deinit(ctx->uart);
+
+  *counter = cnt;
+  return true;
 }
 
-size_t testBaudrate_DMA(size_t baudrate, UART_HandleTypeDef *uart)
-{
-  static size_t counter = 0;
-
-  init_Hal_UART(uart, baudrate);
-  counter = 0;
-  UART_TransferComplete = false;
-  randomizeData(dummy, sizeof(dummy));
-  HAL_StatusTypeDef status = HAL_UART_Transmit_DMA(uart, dummy, sizeof(dummy));
-  if (status != HAL_OK)
-  {
-    // Error !!!
-  }
-  else
-  {
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_SET);
-    while (!UART_TransferComplete)
-    {
-      counter++;
-    }
-    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, GPIO_PIN_RESET);
-  }
-  HAL_UART_DeInit(uart);
-  return counter;
-}
-
-void printResultHAL(size_t *results, size_t size, UART_HandleTypeDef *uart)
-{
-  init_Hal_UART(uart, 9600);
-  for (size_t d = 0; d < size; d++)
-  {
-    static char message[10];
-    sprintf(message, "%d|", results[d]);
-    HAL_UART_Transmit(uart, (uint8_t *)message, strlen(message), 0xffff);
-  }
-  HAL_UART_DeInit(uart);
-}
-
-static size_t baudrates[] = {
-        4800, 9600, 19200, 38400, 57600, 115200, 230400, 460800, 921600,
-    1312500,
-    2625000,
-    5250000,
-    10500000
-    };
+static size_t baudrates[] = {4800,    9600,    19200,   38400,  57600,
+                             115200,  230400,  460800,  921600, 1312500,
+                             2625000, 5250000, 10500000};
 #define TEST_COUNT (sizeof(baudrates) / sizeof(*baudrates))
 #define RETRY 5
 
-void TEST_HAL()
-{
-  static size_t results[TEST_COUNT * RETRY] = {0};
+void TEST_HAL() {
+  char log_buff[128];
   log_message("test_dma\n");
 
-  for (size_t test = 0; test < TEST_COUNT; test++)
-  {
-    for (size_t retry = 0; retry < RETRY; retry++)
-    {
-      results[test * RETRY + retry] = testBaudrateHAL(baudrates[test], &huart1);
-      //results[test*RETRY+retry] = testBaudrate_DMA(baudrates[test],&huart1);
-      char buff[128];
-      sprintf(buff,"%d,%d\n", baudrates[test],results[test*RETRY+retry]);
-      log_message(buff);
+  uint32_t calibration = 0;
+  test_performance(&calibrate_ctx, 0, &calibration);
+  sprintf(log_buff, "Calibartion: %d\n", calibration);
+  log_message(log_buff);
+
+  for (size_t test = 0; test < TEST_COUNT; test++) {
+    for (size_t retry = 0; retry < RETRY; retry++) {
+      uint32_t counter = 0;
+      if (test_performance(&test_dma_ctx, baudrates[test], &counter)) {
+        sprintf(log_buff, "%d,%d\n", baudrates[test], counter);
+        log_message(log_buff);
+      }
     }
   }
-  // printResultHAL(results, TEST_COUNT * RETRY, &huart1);
 }
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -241,9 +276,9 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART1_UART_Init();
-  MX_USART2_UART_Init();
+  // MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-  HAL_UART_DeInit(&huart2);
+  // HAL_UART_DeInit(&huart2);
   HAL_UART_DeInit(&huart1);
 
   TEST_HAL();
@@ -252,8 +287,7 @@ int main(void)
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   log_message("end of program\n");
-  while (1)
-  {
+  while (1) {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -262,20 +296,19 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage 
-  */
+  /** Configure the main internal regulator output voltage
+   */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
+  /** Initializes the CPU, AHB and APB busses clocks
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -284,32 +317,29 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLN = 168;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
-  /** Initializes the CPU, AHB and APB busses clocks 
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  /** Initializes the CPU, AHB and APB busses clocks
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART1_UART_Init(void)
-{
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_USART1_UART_Init(void) {
 
   /* USER CODE BEGIN USART1_Init 0 */
 
@@ -326,54 +356,18 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.Mode = UART_MODE_TX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_8;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
+  if (HAL_UART_Init(&huart1) != HAL_OK) {
     Error_Handler();
   }
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART2_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
-  huart2.Instance = USART2;
-  huart2.Init.BaudRate = 115200;
-  huart2.Init.WordLength = UART_WORDLENGTH_8B;
-  huart2.Init.StopBits = UART_STOPBITS_1;
-  huart2.Init.Parity = UART_PARITY_NONE;
-  huart2.Init.Mode = UART_MODE_TX_RX;
-  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart2.Init.OverSampling = UART_OVERSAMPLING_8;
-  if (HAL_UART_Init(&huart2) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
-}
-
-/** 
-  * Enable DMA controller clock
-  */
-static void MX_DMA_Init(void) 
-{
+ * Enable DMA controller clock
+ */
+static void MX_DMA_Init(void) {
 
   /* DMA controller clock enable */
   __HAL_RCC_DMA2_CLK_ENABLE();
@@ -389,22 +383,19 @@ static void MX_DMA_Init(void)
   /* DMA2_Stream7_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream7_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream7_IRQn);
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-
 }
 
 /* USER CODE BEGIN 4 */
@@ -412,30 +403,30 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
 
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{ 
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line)
+   */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- Src/stm32f4xx_it.c
+++ Src/stm32f4xx_it.c
@@ -209,7 +209,7 @@ void DMA1_Stream5_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Stream5_IRQn 0 */
 
   /* USER CODE END DMA1_Stream5_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart2_rx);
+
   /* USER CODE BEGIN DMA1_Stream5_IRQn 1 */
 
   /* USER CODE END DMA1_Stream5_IRQn 1 */
@@ -223,7 +223,7 @@ void DMA1_Stream6_IRQHandler(void)
   /* USER CODE BEGIN DMA1_Stream6_IRQn 0 */
 
   /* USER CODE END DMA1_Stream6_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_usart2_tx);
+
   /* USER CODE BEGIN DMA1_Stream6_IRQn 1 */
 
   /* USER CODE END DMA1_Stream6_IRQn 1 */
@@ -251,7 +251,7 @@ void USART2_IRQHandler(void)
   /* USER CODE BEGIN USART2_IRQn 0 */
 
   /* USER CODE END USART2_IRQn 0 */
-  HAL_UART_IRQHandler(&huart2);
+
   /* USER CODE BEGIN USART2_IRQn 1 */
 
   /* USER CODE END USART2_IRQn 1 */
